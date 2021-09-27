6d0664c312a9dd8d111cee46d76a0c2050196f82
s7lukabugarin <luka.bugarin@s7codedesign.com>
2021-09-27 17:23:56 +0200

    actions and mutations

--- src/store.js
+++ src/store.js
@@ -1,24 +1,48 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import EventService from '@/services/EventService.js'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
     user: { id: 'abc123', name: 'Luka Bugarin' },
-    categories: ['sustainability', 'nature', 'animal welfare', 'housing', 'education', 'food', 'community'],
+    categories: [
+      'sustainability',
+      'nature',
+      'animal welfare',
+      'housing',
+      'education',
+      'food',
+      'community'
+    ],
+    events: []
   },
-  mutations: {},
-  actions: {},
+  mutations: {
+    ADD_EVENT(state, event) {
+      state.events.push(event)
+    }
+  },
+  actions: {
+    createEvent({ commit }, event) {
+      return EventService.postEvent(event)
+        .then(() => {
+          commit('ADD_EVENT', event)
+        })
+        .catch(() => {
+          console.log('There was a problem creating your event')
+        })
+    }
+  },
   getters: {
     catLength: state => {
-      return state.categories.length;
+      return state.categories.length
     },
     doneTodos: state => {
-      return state.todos.filter(todo => todo.done);
+      return state.todos.filter(todo => todo.done)
     },
-    activeTodosCount: (state) => {
-      return state.todos.filter(todo => !todo.done).length;
-    },
-  },
+    activeTodosCount: state => {
+      return state.todos.filter(todo => !todo.done).length
+    }
+  }
 })
